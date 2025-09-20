ca3023fc14e14485dfdf19ddb0c999c869cc5baf
FreddyPositive <ferdinandthomas918@gmail.com>
2025-09-20 12:12:43 +0530

    Completed the Flash Card app

---
+++ Model/entities/FlashCards.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCardLearning.Model.entities
+{
+    internal class FlashCards
+    {
+        public int Id {get; set;}
+        public string? Question { get; set; }
+        public string? Answer { get; set; }
+        public int StackId { get; set; }
+    }
+}
---
+++ Model/entities/StudySession.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCardLearning.Model.entities
+{
+    internal class StudySession
+    {
+        public string FlashCardsLearned { get; set; }
+        public int StackId { get; set; }
+        public int SessionDuration { get; set; }
+        public int Points { get; set; }
+    }
+
+    internal class StudySessionHistoryInput
+    {
+        public DateOnly SessionStartDate { get; set; }
+        public DateOnly SessionEndDate { get; set; }
+    }
+    internal class StudySessionHistory
+    {
+        public string StackName { get; set; }
+        public DateTime SessionDate { get; set; }
+        public int Points { get; set; }
+    }
+
+    internal class StackReport
+    {
+        public string StackName { get; set; }
+        public int January { get; set; }
+        public int February { get; set; }
+        public int March { get; set; }
+        public int April { get; set; }
+        public int May { get; set; }
+        public int June { get; set; }
+        public int July { get; set; }
+        public int August { get; set; }
+        public int September { get; set; }
+        public int October { get; set; }
+        public int November { get; set; }
+        public int December { get; set; }
+    }
+}
